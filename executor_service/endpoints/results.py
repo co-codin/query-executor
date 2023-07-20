3bd1fcae6a6376ba21e639f97e5d4a5913d5be44
abagyshov <alexander.bagyshov@itentika.ru>
2023-07-20 14:13:02 +0300

    fix endpoints

--- executor_service/endpoints/results.py
+++ executor_service/endpoints/results.py
@@ -1,18 +1,21 @@
 # type: ignore[no-untyped-def]
+import io
 from typing import Dict
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 import pandas as pd
+from starlette.responses import StreamingResponse
+
 from executor_service.dependencies import db_session, get_user
 from executor_service.models.queries import QueryDestination
-
 
 router = APIRouter(
     prefix="/results",
     tags=["results"],
-    responses={404: {"description": "Not found"}},
+    responses={404: {"description": "Not found"}}
 )
+
 
 def available_to_user(reult: QueryDestination, user: dict):
     if user.get('is_superuser'):
@@ -20,11 +23,11 @@ def available_to_user(reult: QueryDestination, user: dict):
     return reult.identity_id == user['identity_id']
 
 
-@router.get("/{guid}", response_model=Dict)
-async def get_result(guid: str, session = Depends(db_session), user = Depends(get_user)):
+@router.get("/{id}", response_model=Dict)
+async def get_result(id: int, session=Depends(db_session), user=Depends(get_user)):
     result = await session.execute(
         select(QueryDestination)
-        .filter(QueryDestination.guid == guid)
+        .filter(QueryDestination.id == id)
     )
     result = result.scalars().first()
     if result is None:
@@ -36,11 +39,11 @@ async def get_result(guid: str, session = Depends(db_session), user = Depends(ge
     return result
 
 
-@router.get('/{guid}/download')
-async def download_result(guid: str, session = Depends(db_session), user = Depends(get_user)):
+@router.get('/{id}/download')
+async def download_result(id: int, session=Depends(db_session), user=Depends(get_user)):
     result = await session.execute(
         select(QueryDestination)
-        .filter(QueryDestination.guid == guid)
+        .filter(QueryDestination.id == id)
     )
     result = result.scalars().first()
     if result is None:
@@ -49,7 +52,16 @@ async def download_result(guid: str, session = Depends(db_session), user = Depen
     if not available_to_user(result, user):
         raise HTTPException(status_code=401)
 
-    df = pd.DataFrame(result)
+    df = pd.DataFrame({
+        "dest_type": [result.dest_type],
+        "status": [result.status],
+        "id": [result.id],
+        "access_creds": [result.access_creds],
+        "query_id": [result.query_id],
+        "path": [result.path],
+        "error_description": [result.error_description],
+        "finished_at": [result.finished_at]
+    })
     stream = io.StringIO()
     df.to_csv(stream, index=False)
 

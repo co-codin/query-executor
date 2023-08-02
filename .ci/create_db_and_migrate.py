55e30143465e82717e9874350f198ad98d0e3477
Антон Ларионов <anton.larionov@itentika.ru>
2023-08-02 18:13:38 +0300

    Update .ci/create_db_and_migrate.py

--- .ci/create_db_and_migrate.py
+++ .ci/create_db_and_migrate.py
@@ -7,6 +7,14 @@ from executor_service.settings import settings
 
 engine = create_engine(settings.db_migration_connection_string)
 
+engine_results = create_engine(settings.db_connection_string_results)
+
+if not database_exists(engine_results.url):
+    print("DB results does not exist, creating...")
+    create_database(engine_results.url)
+else:
+    print("DB  results already exists, skipping")
+
 if not database_exists(engine.url):
     print("DB does not exist, creating...")
     create_database(engine.url)

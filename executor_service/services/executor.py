cc444611ad72f5655636c944d8f706b54fc58a6c
elijah <cuiyeqing960904@gmail.com>
2023-08-07 11:01:17 -0400

    revert

--- executor_service/services/executor.py
+++ executor_service/services/executor.py
@@ -276,8 +276,8 @@ async def _load_into_table(query, write_from):
                 await cursor.execute(ddl)
                 user_name = f'sdwh_run_{query.id}'
                 user_pass = _generate_random_string(8)
-                await cursor.execute(f"CREATE USER {user_name} WITH password %s", [user_pass])
-                await cursor.execute(f"GRANT SELECT ON {table_name} TO {user_name}")
+                # await cursor.execute(f"CREATE USER {user_name} WITH password %s", [user_pass])
+                # await cursor.execute(f"GRANT SELECT ON {table_name} TO {user_name}")
 
                 for batch_records in to_batches(100, reader):
                     await _insert_many(cursor, table_name, names, batch_records)
